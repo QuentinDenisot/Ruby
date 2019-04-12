fc48f7bdf511eb2ed085a3456b68843054360116
Quentin Denisot <quentindenisot@orange.fr>
2019-04-12 19:56:05 +0200

    second commit

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+
+    validates_presence_of :name, :message, :post_id
+    belongs_to :post
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+
+    validates_presence_of :title, :content
+    has_many :comments
+
+end
